1f99526cb6ca7ffe9f350713384d7e49bf22ee3d
Driannn <drianbetancourt@gmail.com>
2024-03-08 21:42:53 -0400

    forgot to save before commit

--- Assets/Scripts/MyHurtBox.gd
+++ Assets/Scripts/MyHurtBox.gd
@@ -8,6 +8,14 @@ func _init() -> void:
 	#Set the mask layer of this hurt box to the number 2
 	#Making it able to detect only the things on the hitbox layer
 	collision_mask = 2
-	
+
 func _ready():
-	pass
+#We use a signal to do something when a HurtBox detect a HitBox
+	#connect("area_entered", self, "_on_area_entered")
+	connect("area_entered", _on_area_entered)
+	
+func _on_area_entered(hitbox: MyHitBox) -> void:
+	if hitbox == null:
+		return
+	
+	
