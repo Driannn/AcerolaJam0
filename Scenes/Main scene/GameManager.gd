d9aea639e35de7c832c71734dd83f582cbd63e71
Driannn <drianbetancourt@gmail.com>
2024-03-09 19:47:11 -0400

    Game manager test
    it suppose to be to change scenes

---
+++ Scenes/Main scene/GameManager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+@onready var player_health = %MyPlayer
+
+func _process(_delta):
+	#print(player_health.health)
+	
+	#if player_health.health <= 0 :
+		#print("change scene")
+	pass
